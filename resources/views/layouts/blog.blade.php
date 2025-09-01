6d46a7f97fd8d3d2992a9f9dfb5e222818cff049
Rey Garcia <jreygarcia24@gmail.com>
2025-09-02 07:10:39 +0800

    fixed footer

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -10,7 +10,7 @@
 </head>
 
 
-<body class="bg-gray-50 dark:bg-gray-900 min-h-screen font-sans text-gray-900 dark:text-gray-100">
+<body class="bg-gray-50 dark:bg-gray-900 min-h-screen font-sans text-gray-900 dark:text-gray-100 flex flex-col">
     @include('components.navigation')
     <header class="bg-gradient-to-r from-blue-600 to-purple-600 py-10 mb-8">
         <div class="max-w-5xl mx-auto px-4">
@@ -18,8 +18,8 @@
             <p class="text-lg text-blue-100">@yield('header-desc', 'Inspiring stories and insights from our writers.')</p>
         </div>
     </header>
-    <main class="max-w-5xl mx-auto px-4 flex flex-col md:flex-row gap-8">
-        <section class="flex-1 min-w-0">
+    <main class="max-w-5xl mx-auto px-4 flex flex-col md:flex-row gap-8 flex-1">
+        <section class="flex-1 min-w-0 md:min-w-96">
             @yield('content')
         </section>
         <aside class="w-full md:w-80 shrink-0">
